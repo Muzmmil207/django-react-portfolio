45ddc9a0cfc37954d35f07d8df1fdf38bec7dd18
mozmmil-ali <mly88207@gmail.com>
2023-06-26 15:26:21 +0200

    update resume

--- frontend/src/components/Projects/ProjectCards.js
+++ frontend/src/components/Projects/ProjectCards.js
@@ -10,13 +10,15 @@ function ProjectCards(props) {
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
+        <Card.Text style={{ textAlign: "justify" }} dangerouslySetInnerHTML={{ __html: props.description }}>
+
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          GitHub
-        </Button>
+        {props.ghLink && (
+          <Button variant="primary" href={props.ghLink} target="_blank">
+            <BsGithub /> &nbsp;
+            {"GitHub"}
+          </Button>
+        )}
         {"\n"}
         {"\n"}
 
