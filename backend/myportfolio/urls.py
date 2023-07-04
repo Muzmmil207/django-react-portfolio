c978f7b691e41befea59bd570bd2a914602f3526
Muzamil Ali <105632991+Muzmmil207@users.noreply.github.com>
2023-07-04 09:01:22 +0200

    Update urls.py

--- backend/myportfolio/urls.py
+++ backend/myportfolio/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     url("admin/", admin.site.urls),
     url(r"^", include("base.urls")),
 ]
-if not settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
