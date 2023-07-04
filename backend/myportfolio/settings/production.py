eb9f2bfd844c606b89c9b1448e2acca0ee286d5c
mozmmil-ali <mly88207@gmail.com>
2023-07-04 09:18:48 +0200

    update production.py

--- backend/myportfolio/settings/production.py
+++ backend/myportfolio/settings/production.py
@@ -21,6 +21,10 @@ if DEBUG:
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+    RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+    if RENDER_EXTERNAL_HOSTNAME:
+        ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
     # Cloudinary Settings For Media Files
     CLOUDINARY_STORAGE = {
         "CLOUD_NAME": env("CLOUD_NAME"),
