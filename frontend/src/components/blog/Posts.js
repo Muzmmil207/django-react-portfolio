2550c78affbfc64928575a809f5c3cacff7092b5
mozmmil-ali <mly88207@gmail.com>
2023-07-30 17:54:44 +0200

    create loading animation

--- frontend/src/components/blog/Posts.js
+++ frontend/src/components/blog/Posts.js
@@ -1,24 +1,32 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Spinner } from "react-bootstrap";
 import PostsCard from "./PostsCards";
 import Particle from "../Particle";
 import React, { useState, useEffect } from "react";
 
 function Posts() {
   const [blogPosts, setBlogPosts] = useState([]);
+  const [loading, setLoading] = useState(true); // Track the loading state
+
   useEffect(() => {
-    getPostsData()
-  }, [])
+    getPostsData();
+  }, []);
 
   let getPostsData = async () => {
-    let response = await fetch('https://muzamil-ali.onrender.com/posts', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
-    let data = await response.json()
-    setBlogPosts(data)
-  }
+    try {
+      let response = await fetch("https://muzamil-ali.onrender.com/posts", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      let data = await response.json();
+      setBlogPosts(data);
+      setLoading(false); // Set loading state to false after successful API response
+    } catch (error) {
+      console.error("Error fetching blog posts:", error);
+      setLoading(false); // Set loading state to false in case of error
+    }
+  };
 
   return (
     <Container fluid className="project-section">
@@ -31,19 +39,23 @@ function Posts() {
           Here are a few posts I've published recently.
         </p>
 
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-
-          {blogPosts.map((post) => (
-            <Col md={4} className="project-card" key={post.id}>
-              <PostsCard
-                imgPath={post.image}
-                title={post.title}
-                poLink={'https://muzamil-blog.netlify.app/blog/' + post.slug}
-              />
-            </Col>
-          ))}
-
-        </Row>
+        {loading ? ( // Render the loading animation if still loading
+          <Spinner animation="border" variant="primary" role="status">
+            <span className="sr-only"></span>
+          </Spinner>
+        ) : (
+          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+            {blogPosts.map((post) => (
+              <Col md={4} className="project-card" key={post.id}>
+                <PostsCard
+                  imgPath={post.image}
+                  title={post.title}
+                  poLink={"https://muzamil-blog.netlify.app/blog/" + post.slug}
+                />
+              </Col>
+            ))}
+          </Row>
+        )}
       </Container>
     </Container>
   );
